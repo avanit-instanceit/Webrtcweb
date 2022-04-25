b4fa5753c26419d0261e9e7c8f0c51deb8831e88
avanit <avanit.diyora@instanceit.com>
2022-04-25 16:18:47 +0530

    25-04-2022

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1fbc9a54b56fc2ba33b2841e12e5875a",
-"index.html": "00aab1749344e8a7bcfb0bfe3b9c842c",
-"/": "00aab1749344e8a7bcfb0bfe3b9c842c",
-"main.dart.js": "f204b48560640b3aa1ce28a916abbbcb",
+"index.html": "88cfaac27f5388caec352e7f4553b64c",
+"/": "88cfaac27f5388caec352e7f4553b64c",
+"main.dart.js": "5d48cde56643960cdf4487e016142c9b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
