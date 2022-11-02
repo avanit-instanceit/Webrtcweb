6e6f7b1b5852f8450b0cb79e4d621f01dc5e7a60
Avanit Diyora <rahul@realtyx.co.in>
2022-11-02 10:47:16 +0530

    2-11-2022

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1fbc9a54b56fc2ba33b2841e12e5875a",
-"index.html": "807875b467f7d1447c8541b2fc2973d5",
-"/": "807875b467f7d1447c8541b2fc2973d5",
+"index.html": "6341e997c4b670b218be48c0047a4a03",
+"/": "6341e997c4b670b218be48c0047a4a03",
 "speechrecognition.js": "095497154509efeb9d5b38d03292b478",
-"main.dart.js": "0df4ee2cb0f931decd6cb61fdf3704c1",
+"main.dart.js": "8199eba50b3dde3193cbd151fbd009c1",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "logo.png": "e3003c9b45351f23c06d2516d4780a20",
