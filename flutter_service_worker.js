050bb4a5807f46d31783a1450c494452e5bbf6c3
Avanit Diyora <rahul@realtyx.co.in>
2023-01-13 13:30:29 +0530

    13-01-23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1fbc9a54b56fc2ba33b2841e12e5875a",
-"index.html": "6b3623f87988751b91f21c27f57d3dbb",
-"/": "6b3623f87988751b91f21c27f57d3dbb",
+"index.html": "78785f6904aeeb309d3b8456ac01395b",
+"/": "78785f6904aeeb309d3b8456ac01395b",
 "speechrecognition.js": "da94096963ca17769ff4397b35c1a0e2",
 "face-api.min.js": "b9fa3888f6ded30c7d01a8f1ed153310",
-"main.dart.js": "33384d08d07fbf6ff55f6e7d2f4151db",
+"main.dart.js": "c2626aad2df904c5ec267bf748f3f435",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "facerecognition.js": "17034aae26f6b3be1dd0bfdc212160db",
 "models/face_landmark_68_tiny_model-shard1": "47047fee26557b55d985952bdfc6cba1",
